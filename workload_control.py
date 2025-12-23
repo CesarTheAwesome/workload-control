3cf462c1f747670105943a0fb4e0ce5eb7cbea9a
CesarTheAwesome <116685922+CesarTheAwesome@users.noreply.github.com>
2025-12-23 15:27:33 -0500

    Import Shift and Job classes and instantiate jobs
    Added imports for Shift and Job classes and instantiated Job objects in main function.

--- workload_control.py
+++ workload_control.py
@@ -2,9 +2,21 @@
 # Cesar Aguirre Elias
 # This File is to run the main program. 
 
+# import shift and job class
+import Shift from shift
+import Job from job
+
 # Day 1: Hardcode two weeks of data.
 # PSA = Patient Service Associate
 # PSA shift -s 8:30am - 5:00pm
 # Valet shift is 9am - 9pm
 Week_1 = {'Monday': 'PSA', 'Tuesday': 'PSA', 'Wednesday': 'PSA', 'Thursday': 'PSA', 'Friday': 'PSA', 'Saturday': 'Off', 'Sunday': 'Off'}
 Week_2 = {'Monday': 'PSA', 'Tuesday': 'PSA', 'Wednesday': 'PSA', 'Thursday': 'PSA', 'Friday': 'PSA', 'Saturday': 'Valet', 'Sunday': 'Valet'}
+
+def main():
+  # instantiate psa and valet job
+  PSA = Job()
+  valet = Job()
+
+if __name__ == "__main__":
+    main()
