a16047b0065f0a10983507ab7e5989bc659dcaa5
CesarTheAwesome <116685922+CesarTheAwesome@users.noreply.github.com>
2025-12-23 14:16:51 -0500

    Add hardcoded shift data for two weeks
    This file contains the main program.
    Initialized file and hardcoded 2 weeks of data.

---
+++ workload_control.py
@@ -0,0 +1,10 @@
+# workload_control.py
+# Cesar Aguirre Elias
+# This File is to run the main program. 
+
+# Day 1: Hardcode two weeks of data.
+# PSA = Patient Service Associate
+# PSA shift -s 8:30am - 5:00pm
+# Valet shift is 9am - 9pm
+Week_1 = {'Monday': 'PSA', 'Tuesday': 'PSA', 'Wednesday': 'PSA', 'Thursday': 'PSA', 'Friday': 'PSA', 'Saturday': 'Off', 'Sunday': 'Off'}
+Week_2 = {'Monday': 'PSA', 'Tuesday': 'PSA', 'Wednesday': 'PSA', 'Thursday': 'PSA', 'Friday': 'PSA', 'Saturday': 'Valet', 'Sunday': 'Valet'}
