cd8832167d882928640c55f4bb15f1f2a154bdb2
CesarTheAwesome <116685922+CesarTheAwesome@users.noreply.github.com>
2025-12-23 15:34:35 -0500

    Update Shift class constructor for attribute initialization
    Refactor Shift class to initialize attributes in __init__ method.

--- shift.py
+++ shift.py
@@ -1,8 +1,10 @@
 # This file contains the Shift class for workload-control.py
 
 class Shift:
-  date = ''
-  start_time = '' 
-  end_time = ''
-  job = ''
-  hours_worked = ''
+  def __init__(self, date, start_time, end_time, job, hours_worked):
+    # Initializes the attributes for a new shift instance
+    date = ''
+    start_time = '' 
+    end_time = ''
+    job = ''
+    hours_worked = ''
