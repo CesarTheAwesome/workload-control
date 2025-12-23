1d72552caf7551f5cd986c90eca3287d60987113
CesarTheAwesome <116685922+CesarTheAwesome@users.noreply.github.com>
2025-12-23 15:42:02 -0500

    Refactor Job class to include constructor
    Updated the Job class constructor to initialize attributes.

--- job.py
+++ job.py
@@ -1,7 +1,8 @@
 # This file contains the Job class for workload-control.py
 
 class Job:
-  job_id = ''
-  title = ''
-  hourly_rate = 0.0
-  default_hours = '' # scheduled hours
+  def __init__(self, job_id, title, hourly_rate):
+    # Initializes the attributes for a new job instance
+    self.job_id = ''
+    self.title = ''
+    self.hourly_rate = 0.0
